2115eb325d361e7f11fc33478b4de3ac94ba620a
Mbasilicato <basilicatom1@southernct.edu>
2024-10-09 00:04:47 +0000

    completed flow control conditions

---
+++ if_statements.py
@@ -0,0 +1,77 @@
+#1
+temp_c = (input("30"))
+if temp_c > 30:
+ print ("it's a hot day")
+else:
+ print("the weather is cool")
+#2
+x = int(input("50"))
+y =int(input("30"))
+if( x > y):
+ print(x)
+elif(y > x):
+ print(y)
+else:
+ print("they're equal")
+#3
+admin_password =("flower")
+user_password = input("what is the password?")
+if (user_password  == admin_password):
+    print("access_granted")
+else:
+    print("access_denied")
+#4
+x=input("pick a number")
+y=input("pick another number")
+operator=input("+,-,*,/")
+if operator == "+":
+    result = x+y
+    print(result)
+elif operator == "-":
+    result = x-y
+    print(x-y)
+elif operator == "*":
+    result = x*y
+    print(x*y)
+elif operator == "/":
+    result = x/y
+    print(x/y)
+#5
+x=input("pick a number")
+if x > 0:
+    print("positive")
+elif x<o:
+    print("negative")
+elif x == 0:
+    print("equals")
+#6
+number =int(input("pick any number 1 to 7"))
+if number == 1:
+    print("Monday")
+elif number == 2:
+    print("Tuesday")
+elif number == 3:
+    print("Wednesdy")
+elif number == 4:
+    print("Thursay")
+elif number == 5:
+    print("Friday")
+elif number == 6:
+    print("Saturday")
+elif number == 7:
+    print("Sunday")
+    
+ 
+
+
+
+
+
+
+ 
+
+    
+
+
+
+
